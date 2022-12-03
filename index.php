24cd5310cc83438383c1d1fa8fa1a77ce393094f
Tamara Demir <tamara.demir@optiweb.com>
2022-12-03 11:33:58 +0100

    d

---
+++ index.php
@@ -0,0 +1,12 @@
+
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+</head>
+
+<body testtest </body>
+</html>
+
