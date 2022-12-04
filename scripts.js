c27c45f3e5139b9a4d41b345cc54bfbec0f5e4a6
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 13:28:50 +0100

    Change color on click

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
-function changeColor() {
-    alert('test');
-}
+// First we detect the click event
+document.getElementsByClassName('the-box').addEventListener('click', function () {
+        this.classList.add('selected');
+});
+
+let questions = document.getElementsByClassName("question");
+
 
     fetch('Data/questions.json')
         .then((response) => response.json())
         .then((questions) => console.log(questions));
+
+
