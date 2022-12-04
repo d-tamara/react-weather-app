b1ff449cf6336d231c8a8ba4384444dced5fd0d7
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 14:17:57 +0100

    Change color on click

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-let question1 = document.querySelector('#question-1');
-let question2 = document.querySelector('#question-2');
+let question1 = document.getElementById('#question-1');
+let question2 = document.getElementById('#question-2');
 
 question1.addEventListener('click', () => question1.style.backgroundColor='#3399ff')
 question2.addEventListener('click', () => question2.style.backgroundColor='#3399ff')
