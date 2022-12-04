eedf630b8426cf91d7e7bfd61611b11dd475816d
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 23:30:31 +0100

    update json

--- scripts.js
+++ scripts.js
@@ -76,6 +76,7 @@ window.onload = function () {
 
 
     document.getElementById('nextQuestion').addEventListener('click', function () {
+        console.log(questions.length);
         if(id < questions.length) {
             this.classList.remove('show-element');
             this.classList.add('hide-element');
