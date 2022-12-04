e2e87e21f5908f5b56bf7b210e86c02735b6e205
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 12:30:33 +0100

    Initial commit

--- scripts.js
+++ scripts.js
@@ -2,25 +2,6 @@ function changeColor() {
     alert('test');
 }
 
-
-
-/*
     fetch('Data/questions.json')
         .then((response) => response.json())
-        .then((questions) => console.log(questions));*/
-
-
-
-function getvals(){
-    return fetch('Data/questions.json')
-        .then((response) => response.json())
-
-}
-
-async function main() {
-    const data = await getvals(); // Waits here until getvals() is done.
-    console.log(data.firstQuestion);
-}
-
-main();
-
+        .then((questions) => console.log(questions));
