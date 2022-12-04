18951a7e4baadc8b7d567938657903b5438aabae
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 11:29:32 +0100

    Initial commit

--- scripts.js
+++ scripts.js
@@ -2,5 +2,8 @@ function changeColor() {
     alert('test');
 }
 
-import data from '/Data/questions.json' assert { type: 'JSON' };
+import data from '/Data/questions.json' assert { type: 'json' };
 console.log(data);
+
+
+console.log(data.firstQuestion);
