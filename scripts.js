5987cde333a42afdf7d698a3b019305ca1e3a53a
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 14:25:14 +0100

    Change color on click

--- scripts.js
+++ scripts.js
@@ -1,17 +1,20 @@
-/*let question1 = document.getElementById('#question-1');
+fetch('Data/questions.json')
+    .then((response) => response.json())
+    .then((questions) => console.log(questions));
+
+
+let question1 = document.getElementById('#question-1');
 let question2 = document.getElementById('#question-2');
 if(queston1) {
     question1.addEventListener('click', () => question1.style.backgroundColor='#3399ff')
 }
 if(question2) {
     question2.addEventListener('click', () => question2.style.backgroundColor='#3399ff')
-}*/
+}
 
 
 
 
-    fetch('Data/questions.json')
-        .then((response) => response.json())
-        .then((questions) => console.log(questions));
+
 
 
