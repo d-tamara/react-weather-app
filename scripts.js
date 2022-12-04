1b970ac400015e40912b2af5317ef8ae88679ea3
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 17:18:32 +0100

    fetch api

--- scripts.js
+++ scripts.js
@@ -39,8 +39,8 @@ window.onload = function () {
 
     let question1 = document.getElementById('question-1');
     let question2 = document.getElementById('question-2');
-    question1.addEventListener('click', () => question1.classList.add = 'selected');
-    question2.addEventListener('click', () => question2.classList.add = 'selected');
+    question1.addEventListener('click', () => question1.classList.add('selected');
+    question2.addEventListener('click', () => question2.classList.add('selected');
 
 
 };
