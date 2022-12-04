2304e5cb275c5b0bf4fdf2b5e68dd5c465767d5a
Tamara Demir <tamara.demir@optiweb.com>
2022-12-04 17:14:20 +0100

    fetch api

--- scripts.js
+++ scripts.js
@@ -39,13 +39,8 @@ window.onload = function () {
 
     let question1 = document.getElementById('question-1');
     let question2 = document.getElementById('question-2');
-    if (question1) {
-        alert('test');
-        question1.addEventListener('click', () => question1.style.backgroundColor = '#3399ff')
-    }
-    if (question2) {
-        question2.addEventListener('click', () => question2.style.backgroundColor = '#3399ff')
-    }
+    question1.addEventListener('click', () => question1.classList.add = 'selected');
+    question2.addEventListener('click', () => question2.classList.add = 'selected');
 
 
 };
