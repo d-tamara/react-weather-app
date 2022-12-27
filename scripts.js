f719bd0b6285cd6903d96be93542955c4a953d79
Tamara Demir <tamara.demir@optiweb.com>
2022-12-27 22:38:01 +0100

    Konfigurator

--- scripts.js
+++ scripts.js
@@ -1,126 +1,51 @@
 window.onload = function () {
 
-    async function fetchQuestions() {
+    let podnozje1 = document.getElementById('noge');
+    let podnozje2 = document.getElementById('naslonjala');
+    let chairImage = document.getElementById('chairImage');
 
-        const response = await fetch(('https://api.jsonbin.io/v3/b/63847bc2a3c728450ed954c1'), {
-            method: 'GET',
-            headers: {
-                'X-Master-Key': '$2b$10$cwaQJ/5tvKVQ7qtRl/jNPOaDwhRdayBU4ENJ0Pyix3wwUhOirs0Yy',
-                'X-Bin-Private': 'false'
-            },
-        });
-        const questions = await response.json();
-        console.log(questions.record);
-        return questions.record;
+    function selectOption(p1, p2, id1, id2) {
+        p1.classList.remove('hide-element');
+        p1.classList.add('show-element');
+        p2.classList.add('hide-element');
+        p2.classList.remove('show-element');
+        p1.children[0].classList.remove('selected');
+        p1.children[1].classList.remove('selected');
+        chairImage.classList.add('hide-element');
+        document.getElementById(id1).classList.add('selected');
+        document.getElementById(id2).classList.remove('selected');
     }
 
-    async function updateAnswers() {
-
-        await fetch(('https://api.jsonbin.io/v3/b/63847bc2a3c728450ed954c1'), {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Master-Key': '$2b$10$cwaQJ/5tvKVQ7qtRl/jNPOaDwhRdayBU4ENJ0Pyix3wwUhOirs0Yy',
-                'X-Bin-Private': 'false'
-            },
-            body: JSON.stringify(questions),
-        })
-            .then((response) => response.json())
-            .then((questions) => {
-                console.log('Success:', questions);
-            });
+    function showSelected(id1, id2, imageID) {
+        document.getElementById(id1).classList.add('selected');
+        document.getElementById(id2).classList.remove('selected');
+        chairImage.classList.remove('hide-element');
+        chairImage.src = imageID;
     }
 
-
-    let id = 0;
-    let firstAnswerSelected = 0;
-    let secondAnswerSelected = 0;
-    let allAnswers = 0;
-
-    let questions = [];
-
-    async function renderQuestions(id) {
-        console.log(questions.length);
-        if(questions.length == 0) {
-            questions = await fetchQuestions();
-        }
-
-        let firstPair = questions[id];
-        document.getElementById('question-1-text').innerHTML = firstPair.firstQuestion;
-        document.getElementById('question-2-text').innerHTML = firstPair.secondQuestion;
-        firstAnswerSelected = firstPair.firstAnswerSelected;
-        secondAnswerSelected = firstPair.secondAnswerSelected;
-        allAnswers = firstAnswerSelected + secondAnswerSelected;
-    }
-
-    renderQuestions(id);
-
-
-    let isSelected = false;
-
-    var buttonNextQuestion = document.getElementById('nextQuestion');
-    buttonNextQuestion.classList.add('hide-element');
-
-
-    document.getElementById('question-1').addEventListener('click', function () {
-        if(!isSelected) {
-            this.classList.add('selected');
-            if(id < questions.length - 1) {
-                buttonNextQuestion.classList.remove('hide-element');
-                buttonNextQuestion.classList.add('show-element');
-            }
-            isSelected = true;
-            firstAnswerSelected++;
-            allAnswers++;
-            document.getElementById('percentage-1').innerHTML = toPercentage(firstAnswerSelected, allAnswers);
-            document.getElementById('percentage-2').innerHTML = toPercentage(secondAnswerSelected, allAnswers);
-            questions[id].firstAnswerSelected += 1;
-
-            updateAnswers();
-
-        }
+    document.getElementById('podnozje-1').addEventListener('click', function () {
+        selectOption(podnozje1, podnozje2, 'podnozje-1', 'podnozje-2');
+        showSelected('noge-1', 'noge-2', './Data/main-a-1.png');
     });
 
-
-    document.getElementById('question-2').addEventListener('click', function () {
-        if(!isSelected) {
-            this.classList.add('selected');
-            if(id < questions.length - 1) {
-                buttonNextQuestion.classList.remove('hide-element');
-                buttonNextQuestion.classList.add('show-element');
-            }
-            isSelected = true;
-            secondAnswerSelected++;
-            allAnswers++;
-            document.getElementById('percentage-1').innerHTML = toPercentage(firstAnswerSelected, allAnswers);
-            document.getElementById('percentage-2').innerHTML = toPercentage(secondAnswerSelected, allAnswers);
-            questions[id].secondAnswerSelected += 1;
-
-            updateAnswers();
-        }
+    document.getElementById('podnozje-2').addEventListener('click', function () {
+        selectOption(podnozje2, podnozje1, 'podnozje-2','podnozje-1');
+        showSelected('naslonjalo-1', 'naslonjalo-2', './Data/main-b-1.png');
     });
 
-
-    document.getElementById('nextQuestion').addEventListener('click', function () {
-        if(id < questions.length) {
-            this.classList.remove('show-element');
-            this.classList.add('hide-element');
-            isSelected = false;
-            id++;
-            document.getElementById('percentage-1').innerHTML = '';
-            document.getElementById('percentage-2').innerHTML = '';
-
-            document.getElementById('question-1').classList.remove('selected');
-            document.getElementById('question-2').classList.remove('selected');
-            renderQuestions(id);
-        }
+    document.getElementById('noge-1').addEventListener('click', function () {
+        showSelected('noge-1', 'noge-2', './Data/main-a-1.png');
     });
 
+    document.getElementById('noge-2').addEventListener('click', function () {
+        showSelected('noge-2', 'noge-1', './Data/main-a-2.png');
+    });
 
+    document.getElementById('naslonjalo-1').addEventListener('click', function () {
+        showSelected('naslonjalo-1', 'naslonjalo-2', './Data/main-b-1.png');
+    });
+
+    document.getElementById('naslonjalo-2').addEventListener('click', function () {
+        showSelected('naslonjalo-2', 'naslonjalo-1', './Data/main-b-2.png');
+    });
 };
-
-
-function toPercentage(thisAnswerCounter, allAnswersCounter) {
-    let percentage = (thisAnswerCounter/allAnswersCounter) * 100;
-    return Math.trunc(percentage.toString()) + '%';
-}
